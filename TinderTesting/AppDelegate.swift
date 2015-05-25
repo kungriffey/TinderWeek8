56199bd920e5e94e31a5ff3e04c7d6164f3bd0dd
kunwardeep <kunwardeep.gill@gmail.com>
2015-05-25 15:18:42 -0400

    added Parse

--- TinderTesting/AppDelegate.swift
+++ TinderTesting/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     // Override point for customization after application launch.
-    
+    Parse.setApplicationId("nFy65AmJYVXq9dgkaeclr2I0BxVrskCTw67EQzBP", clientKey: "vvdbvhlMFRAnKxOE20pCA5uIOSopu8DZTKHPIbh5")
     
     return true
   }
---
+++ TinderTesting/BridgingFile.m
@@ -0,0 +1,9 @@
+//
+//  BridgingFile.m
+//  TinderTesting
+//
+//  Created by Kunwardeep Gill on 2015-05-25.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
