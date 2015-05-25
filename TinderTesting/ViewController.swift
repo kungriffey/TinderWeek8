6faa10acde459773626d9864363cb3942caa9bcb
kunwardeep <kunwardeep.gill@gmail.com>
2015-05-25 14:03:33 -0400

    Initial Commit

---
+++ TinderTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinderTesting
+//
+//  Created by Kunwardeep Gill on 2015-05-25.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
